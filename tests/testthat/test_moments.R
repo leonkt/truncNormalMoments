e05e4e83ae15c422439dbf373a40c1a4f0fb58f8
Maya Mathur <mmathur@stanford.edu>
2021-11-10 12:03:04 -0800

    stop tests for now

--- tests/testthat/test_moments.R
+++ tests/testthat/test_moments.R
@@ -7,6 +7,7 @@ library(rstan)
 stancode <- 'data {real y_mean;} parameters {real y;} model {y ~ normal(y_mean,1);}'
 mod <- stan_model(model_code = stancode, verbose = TRUE)
 fit <- sampling(mod, data = list(y_mean = 0))
+# I added this line, and it's the culprit
 summary(fit)$summary
 
 
