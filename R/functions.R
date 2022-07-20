f01e2e5ff4ad87aad372e4a67ba197d94a59fbd5
Mika Braginsky <mika.br@gmail.com>
2022-07-20 17:37:12 -0400

    style code and docs

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,5 @@
-################################ INTERMEDIATE TERMS ################################
-
-#' Return the conditional density of a normal random variable, given that its value is between a and b, evaluated at Za.
+#' Return the conditional density of a normal random variable, given that its
+#' value is between a and b, evaluated at z_a.
 #'
 #' @param mean Mean of normal distribution.
 #' @param sd Standard deviation of normal distribution.
@@ -16,15 +15,16 @@ alpha_a <- function(mean, sd, a, b) {
   stopifnot(a < b)
   stopifnot(sd > 0)
 
-  z_score <- function(mean, sd, x){ (x-mean)/sd }
+  z_score <- function(mean, sd, x) (x - mean) / sd
 
-  Za = z_score(mean, sd, a)
-  Zb = z_score(mean, sd, b)
+  z_a <- z_score(mean, sd, a)
+  z_b <- z_score(mean, sd, b)
 
-  dnorm(Za) / (pnorm(Zb) - pnorm(Za))
+  dnorm(z_a) / (pnorm(z_b) - pnorm(z_a))
 }
 
-#' Return the conditional density of a normal random variable, given that its value is between a and b, evaluated at Zb.
+#' Return the conditional density of a normal random variable, given that its
+#' value is between a and b, evaluated at z_b.
 #'
 #' @param mean Mean of normal distribution.
 #' @param sd Standard deviation of normal distribution.
@@ -40,12 +40,12 @@ alpha_b <- function(mean, sd, a, b) {
   stopifnot(a < b)
   stopifnot(sd > 0)
 
-  z_score <- function(mean, sd, x){ (x-mean)/sd }
+  z_score <- function(mean, sd, x) (x - mean) / sd
 
-  Za = z_score(mean, sd, a)
-  Zb = z_score(mean, sd, b)
+  z_a <- z_score(mean, sd, a)
+  z_b <- z_score(mean, sd, b)
 
-  dnorm(Za) / (pnorm(Zb) - pnorm(Za))
+  dnorm(z_a) / (pnorm(z_b) - pnorm(z_a))
 }
 
 
@@ -65,40 +65,38 @@ alpha_b <- function(mean, sd, a, b) {
 nlpost_jeffreys <- function(mean, sd, x, a, b) {
   stopifnot(a < b)
 
-  if ( sd < 0 ) return(.Machine$integer.max)
+  if (sd < 0) return(.Machine$integer.max)
 
   # as in nll()
   term1 <- mvtnorm::dmvnorm(x = as.matrix(x, nrow = 1),
                             mean = as.matrix(mean, nrow = 1),
-                            # sigma here is covariance matrix,
-                            sigma = as.matrix(sd^2, nrow=1),
+                            # sigma here is covariance matrix
+                            sigma = as.matrix(sd ^ 2, nrow = 1),
                             log = TRUE)
 
+  # remember sigma here is covariance matrix, not the SD
+  term2 <- length(x) * log(mvtnorm::pmvnorm(lower = a,
+                                            upper = b,
+                                            mean = mean,
+                                            sigma = sd ^ 2))
 
-  term2 <- length(x) * log( mvtnorm::pmvnorm(lower = a,
-                                             upper = b,
-                                             mean = mean,
-                                             # remember sigma here is covariance matrix, not the SD
-                                             sigma = sd^2 ) )
+  ef <- e_fisher(mean = mean, sd = sd, n = length(x), a = a, b = b)
+  term3 <- log(sqrt(det(ef)))
 
-  term3 <- log( sqrt( det( e_fisher(mean = mean, sd = sd, n = length(x), a = a, b = b) ) ) )
+  nlp_value <- -(sum(term1) - term2 + term3)
 
-  nlp.value <- -( sum(term1) - term2 + term3 )
-
-  if ( is.infinite(nlp.value) | is.na(nlp.value) ) {
-    return(.Machine$integer.max)
-  }
-
-  nlp.value
-
+  if (is.infinite(nlp_value) || is.na(nlp_value)) return(.Machine$integer.max)
+  return(as.numeric(nlp_value))
 }
 
-#' Estimates the posterior modes for the parameters of the underlying normal distribution, given truncated data
+#' Estimates the posterior modes for the parameters of the underlying normal
+#' distribution, given truncated data
 #'
 #' @param x Vector of observations from truncated normal
-#' @param mean.start Initial value for mu.
-#' @param sd.start Initial value for sigma.
-#' @param ci.level Number between 0.5 and 1. Gives a 100(ci.level)% confidence interval.
+#' @param mean_start Initial value for mu.
+#' @param sd_start Initial value for sigma.
+#' @param ci_level Number between 0.5 and 1. Gives a 100(ci_level)% confidence
+#'   interval.
 #' @param a Left truncation limit.
 #' @param b Right truncation limit.
 #' @param ... Parameters to pass to sampling()
@@ -107,21 +105,22 @@ nlpost_jeffreys <- function(mean, sd, x, a, b) {
 #' @export
 #'
 #' @examples
-#' trunc_est(x = c(-1,-2,1,2), mean.start = 1, sd.start = 0.5, ci.level = 0.975, a = 1, b = 5)
+#' trunc_est(x = c(-1,-2,1,2), mean_start = 1, sd_start = 0.5, ci_level = 0.975,
+#'           a = 1, b = 5)
 #'
 #' @references
 #' https://mc-stan.org/rstan/reference/stanmodel-method-sampling.html
 trunc_est <- function(x,
-                      mean.start = 0,
-                      sd.start = 1,
-                      ci.level = 0.95,
+                      mean_start = 0,
+                      sd_start = 1,
+                      ci_level = 0.95,
                       a,
                       b,
                       ...) {
   stopifnot(a < b)
-  stopifnot(sd.start > 0)
+  stopifnot(sd_start > 0)
 
-  model.text <- "
+  model_text <- "
 functions{
 	real jeffreys_prior(real mu, real sigma, real a, real b, int n){
 		real mustarL;
@@ -188,99 +187,102 @@ generated quantities{
 "
 
 # prepare to capture warnings from Stan
-stan.warned = 0
-stan.warning = NA
+stan_warned <- 0
+stan_warning <- NA
 
 # set start values for sampler
-init.fcn <- function(o){ list(mean=mean.start, sd=sd.start) }
+init_fcn <- function(o) list(mean = mean_start, sd = sd_start)
 
 # like tryCatch, but captures warnings without stopping the function from
 #  returning its results
 withCallingHandlers({
 
-  # "isystem" arg is just a placeholder to avoid Stan's not understanding special characters
-  #  in getwd(), even though we don't actually use the dir at all
-  stan.model <- rstan::stan_model(model_code = model.text,
+  # "isystem" arg is just a placeholder to avoid Stan's not understanding
+  # special characters in getwd(), even though we don't actually use the dir at
+  # all
+  stan_model <- rstan::stan_model(model_code = model_text,
                                   isystem = "~/Desktop")
 
 
-  post <- rstan::sampling(stan.model,
+  post <- rstan::sampling(stan_model,
                           cores = 1,
                           #refresh = 0,
-                          init = init.fcn, data=list(n=length(x), a=a, b=b, y=x), ...)
+                          init = init_fcn,
+                          data = list(n = length(x), a = a, b = b, y = x), ...)
 
 
-}, warning = function(condition){
-  stan.warned <<- 1
-  stan.warning <<- condition$message
-} )
+}, warning = function(condition) {
+  stan_warned <<- 1
+  stan_warning <<- condition$message
+})
 
 ext <- rstan::extract(post)
-best.ind = which.max(ext$log_post)
+best_ind <- which.max(ext$log_post)
 
+post_summ <- summary(post)$summary
 
-postSumm <- summary(post)$summary
+# nlpost_simple <- function(mean, sd) nlpost_jeffreys(mean, sd, x, a, b)
 
-print(postSumm)
-nlpost_simple = function(mean, sd) {
-  nlpost.value = nlpost_jeffreys(mean, sd, x, a, b)
-  return(nlpost.value)
-}
+# res <- stats4::mle(minuslogl = nlpost_simple,
+#                    start = list(mean = ext$mu[best_ind],
+#                                 sd = ext$sigma[best_ind]),
+#                    method = "Nelder-Mead")
 
-
-res <- stats4::mle( minuslogl = nlpost_simple,
-                    start = list(mean=ext$mu[best.ind], sd= ext$sigma[best.ind]), method="Nelder-Mead" )
-
-
-maps <- as.numeric(stats4::coef(res))
+# maps <- as.numeric(stats4::coef(res))
 
 # posterior means, then medians
-mean.est <- median(rstan::extract(post, "mu")[[1]])
-sd.est <- median(rstan::extract(post, "sigma")[[1]])
+mean_est <- median(rstan::extract(post, "mu")[[1]])
+sd_est <- median(rstan::extract(post, "sigma")[[1]])
 
-mean.maxlp <- ext$mu[best.ind]
-sd.maxlp <- ext$sigma[best.ind]
+mean_maxlp <- ext$mu[best_ind]
+sd_maxlp <- ext$sigma[best_ind]
 # SEs
-mean.se = postSumm["mu", "se_mean"]
-sd.se = postSumm["sigma", "se_mean"]
+mean_se <- post_summ["mu", "se_mean"]
+sd_se <- post_summ["sigma", "se_mean"]
 
-# convert the numeric ci.level to strings of percentages.
-l.lim.str <- paste0(toString((1-ci.level) * 100), "%")
-r.lim.str <- paste0(toString((ci.level) * 100), "%")
+# convert the numeric ci_level to strings of percentages.
+# l.lim.str <- paste0(toString((1 - ci_level) * 100), "%")
+# r.lim.str <- paste0(toString((ci_level) * 100), "%")
 
 # CI limits
-sd.ci.lims = c( postSumm["sigma", l.lim.str], postSumm["sigma", r.lim.str] )
-mean.ci.lims = c( postSumm["mu", l.lim.str], postSumm["mu", r.lim.str] )
+# sd_ci_lims <- c(post_summ["sigma", l.lim.str], post_summ["sigma", r.lim.str])
+# mean_ci_lims <- c(post_summ["mu", l.lim.str], post_summ["mu", r.lim.str])
+
+mean_ci <- as.numeric(
+  c(quantile(rstan::extract(post, "mu")[[1]], 1 - ci_level),
+    quantile(rstan::extract(post, "mu")[[1]], ci_level))
+)
+
+sd_ci <- as.numeric(
+  c(quantile(rstan::extract(post, "sigma")[[1]], 1 - ci_level),
+    quantile(rstan::extract(post, "sigma")[[1]], ci_level))
+)
 
 
-mean.ci = as.numeric( c( quantile( rstan::extract(post, "mu")[[1]], 1-ci.level ),
-                         quantile( rstan::extract(post, "mu")[[1]], ci.level ) ) )
+# the point estimates are length 2 (post means, then medians), but the inference
+# is the same for each type of point estimate
 
-sd.ci = as.numeric( c( quantile( rstan::extract(post, "sigma")[[1]], 1-ci.level ),
-                       quantile( rstan::extract(post, "sigma")[[1]], ci.level ) ) )
-# the point estimates are length 2 (post means, then medians),
-#  but the inference is the same for each type of point estimate
-return( list( post = post,
-              mean.est = mean.est,
-              sd.est = sd.est,
+return(list(post = post,
+            mean_est = mean_est,
+            sd_est = sd_est,
 
-              mean.maxlp = mean.maxlp,
-              sd.maxlp = sd.maxlp,
-              mean.se = rep(mean.se, 2),
-              sd.se= rep(sd.se, 2),
+            mean_maxlp = mean_maxlp,
+            sd_maxlp = sd_maxlp,
+            mean_se = rep(mean_se, 2),
+            sd_se = rep(sd_se, 2),
 
-              mean.ci = mean.ci,
-              sd.ci = sd.ci,
+            mean_ci = mean_ci,
+            sd_ci = sd_ci,
 
-              stan.warned = stan.warned,
-              stan.warning = stan.warning,
+            stan_warned = stan_warned,
+            stan_warning = stan_warning,
 
-              mean.rhat = postSumm["mu", "Rhat"],
-              sd.rhat = postSumm["sigma", "Rhat"]
-) )
+            mean_rhat = post_summ["mu", "Rhat"],
+            sd_rhat = post_summ["sigma", "Rhat"]))
 }
 
-#' Finds the Fisher information matrix contained in n samples from a truncated normal distribution.
+#' Finds the Fisher information matrix contained in n samples from a truncated
+#' normal distribution.
 
 #' @param mean Mean of underlying normal distribution.
 #' @param sd Standard deviation of underlying normal distribution.
@@ -297,25 +299,25 @@ e_fisher <- function(mean, sd, n, a, b) {
   stopifnot(sd > 0)
   stopifnot(a < b)
 
-  Za = (a - mean) / sd
-  Zb = (b - mean) / sd
+  z_a <- (a - mean) / sd
+  z_b <- (b - mean) / sd
 
-  alpha.a = alpha_a(mean, sd, a, b)
-  alpha.b = alpha_b(mean, sd, a, b)
+  alpha_a <- alpha_a(mean, sd, a, b)
+  alpha_b <- alpha_b(mean, sd, a, b)
 
-  k11 = -(n/sd^2) + (n/sd^2)*( (alpha.b - alpha.a)^2 + (alpha.b*Zb - alpha.a*Za) )
+  k11 <- -(n / sd ^ 2) + (n / sd ^ 2) * ((alpha_b - alpha_a) ^ 2 +
+                                           (alpha_b * z_b - alpha_a * z_a))
 
-  k12 = -( 2*n*(alpha.a - alpha.b) / sd^2 ) +
-    (n/sd^2)*( alpha.a - alpha.b + alpha.b*Zb^2 - alpha.a*Za^2 +
-                 (alpha.a - alpha.b)*(alpha.a*Za - alpha.b*Zb) )
+  k12 <- -(2 * n * (alpha_a - alpha_b) / sd ^ 2) +
+    (n / sd ^ 2) * (alpha_a - alpha_b + alpha_b * z_b ^ 2 - alpha_a * z_a ^ 2 +
+                      (alpha_a - alpha_b) * (alpha_a * z_a - alpha_b * z_b))
 
-  k22 = (n/sd^2) - (3*n*(1 + alpha.a*Za - alpha.b*Zb) / sd^2) +
-    (n/sd^2)*( Zb*alpha.b*(Zb^2 - 2) - Za*alpha.a*(Za^2 - 2) +
-                 (alpha.b*Zb - alpha.a*Za)^2 )
+  k22 <- (n / sd ^ 2) - (3 * n * (1 + alpha_a * z_a - alpha_b * z_b) / sd ^ 2) +
+    (n / sd ^ 2) * (z_b * alpha_b * (z_b ^ 2 - 2) - z_a * alpha_a *
+                      (z_a ^ 2 - 2) +
+                      (alpha_b * z_b - alpha_a * z_a) ^ 2)
 
-  return( matrix( c(-k11, -k12, -k12, -k22),
-                  nrow = 2,
-                  byrow = TRUE ) )
+  return(matrix(c(-k11, -k12, -k12, -k22), nrow = 2, byrow = TRUE))
 }
 
 #' Returns the value of the Jeffreys prior.
@@ -333,5 +335,6 @@ prior_jeffreys <- function(mean, sd, x, a, b) {
   stopifnot(a < b)
   stopifnot(sd > 0)
 
-  return (log( sqrt( det( e_fisher(mean = mean, sd = sd, n = length(x), a = a, b = b) ) ) ))
+  ef <- e_fisher(mean = mean, sd = sd, n = length(x), a = a, b = b)
+  return(log(sqrt(det(ef))))
 }
