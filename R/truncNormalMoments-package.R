48519441bdac1298ef09adceb7070211a51035cf
Mika Braginsky <mika.br@gmail.com>
2022-07-25 19:12:08 -0400

    add references

--- R/truncNormalMoments-package.R
+++ R/truncNormalMoments-package.R
@@ -1,5 +1,15 @@
+#' @keywords internal
+#' @references
+#' \insertRef{zhou2014}{truncNormalMoments}
+#'
+#' \insertRef{stan2022}{truncNormalMoments}
+"_PACKAGE"
+
+## usethis namespace: start
 #' @useDynLib truncNormalMoments, .registration = TRUE
 #' @import methods
 #' @import Rcpp
+#' @importFrom Rdpack reprompt
 #' @importFrom stats dnorm pnorm median quantile
+## usethis namespace: end
 NULL
