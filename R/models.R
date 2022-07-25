8e38127a7efabc80bf1f09ef447a42625de7ba43
Mika Braginsky <mika.br@gmail.com>
2022-07-25 19:13:47 -0400

    return mode, document return value

--- R/models.R
+++ R/models.R
@@ -1,5 +1,7 @@
+#' Truncated normal estimation
+#'
 #' Estimates the posterior modes for the parameters of the underlying normal
-#' distribution, given truncated data
+#' distribution, given truncated data.
 #'
 #' @param x Vector of observations from truncated normal
 #' @param mean_start Initial value for mu.
@@ -10,20 +12,30 @@
 #' @param b Right truncation limit.
 #' @param ... Parameters to pass to sampling()
 #'
+#' @return A list with two elements:
+#'  \describe{
+#'    \item{stats}{A data frame with two rows and the columns \code{param}
+#'                 (\code{mean}, \code{sd}), \code{mean} (posterior mean),
+#'                 \code{median} (posterior median), \code{maxlp}, \code{se},
+#'                 \code{ci_lower}, \code{ci_upper}, \code{rhat}.}
+#'    \item{fit}{A \code{stanfit} object (the result of fitting the model).}
+#'  }
+#'
 #' @export
 #'
-#' @examples
-#' x <- truncnorm::rtruncnorm(100, a = 0, b = 2, mean = 0.5, sd = 0.5)
-#' trunc_est(x, a = 0, b = 2)
-#'
 #' @references
-#' https://mc-stan.org/rstan/reference/stanmodel-method-sampling.html
+#' \insertRef{zhou2014}{truncNormalMoments}
+#' \insertRef{stan2022}{truncNormalMoments}
+#'
+#' @examples
+#' x <- truncnorm::rtruncnorm(100, a = -1, b = 2, mean = 0.5, sd = 0.5)
+#' trunc_est(x, a = -1, b = 2)
 trunc_est <- function(x,
+                      a,
+                      b,
                       mean_start = 0,
                       sd_start = 1,
                       ci_level = 0.95,
-                      a,
-                      b,
                       ...) {
   stopifnot(a < b)
   stopifnot(sd_start > 0)
@@ -50,15 +62,15 @@ trunc_est <- function(x,
   medians <- c(median(stan_extract$mu), median(stan_extract$sigma))
 
   index_maxlp <- which.max(stan_extract$log_post)
-  maxlps <- c(stan_extract$mu[index_maxlp], stan_extract$sigma[index_maxlp])
+  modes <- c(stan_extract$mu[index_maxlp], stan_extract$sigma[index_maxlp])
 
   stan_ci <- function(param, q) as.numeric(quantile(stan_extract[[param]], q))
   cil <- c(stan_ci("mu", 1 - ci_level), stan_ci("sigma", 1 - ci_level))
   ciu <- c(stan_ci("mu", ci_level), stan_ci("sigma", ci_level))
 
-  stan_stats <- data.frame(param = c("mean", "sd"), mean = means,
-                           median = medians, maxlp = maxlps, se = ses,
-                           ci_lower = cil, ci_upper = ciu, rhat = rhats)
+  stan_stats <- data.frame(param = c("mu", "sigma"), mode = modes, mean = means,
+                           median = medians, se = ses, ci_lower = cil,
+                           ci_upper = ciu, rhat = rhats)
 
   return(list(stats = stan_stats, fit = stan_fit))
 }
